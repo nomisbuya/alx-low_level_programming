fa2cb898e6a712916f2cc70b939338d7da7cd51b
nomisbuya <simonayub@gmail.com>
2023-02-17 09:09:59 -0800

    bigaplphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - prints alphabet in l and uper
+ * Return: (0) Always
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
