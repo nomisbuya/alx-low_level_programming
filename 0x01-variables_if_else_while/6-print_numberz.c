cf8493a828f46821ad7373cc00099641032d266b
nomisbuya <simonayub@gmail.com>
2023-02-17 09:49:32 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - prints intergers 0 to 10
+ * Return: (0) Always
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
